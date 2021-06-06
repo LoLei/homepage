eba22e8f6011452674a9ccce540e05e4ebac9295
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-06 19:43:16 +0200

    Add header and footer components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import "../styles/all.scss";
 import type { AppProps } from "next/app";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="container">
       <div className="header">
-        <header>Header</header>
+        <Header></Header>
       </div>
       <div className="main">
         <Component {...pageProps} />
       </div>
       <div className="footer">
-        <footer>Footer</footer>
+        <Footer>Footer</Footer>
       </div>
     </div>
   );
