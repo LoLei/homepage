4ca079790f7ad44ca6caf4c7eb45aecb968fd344
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-07 20:18:26 +0200

    Add about boilerplate text

--- components/About.tsx
+++ components/About.tsx
@@ -8,8 +8,15 @@ const About = (): JSX.Element => {
 
 Hi, I’m Lorenz. Here are some facts about me that might be of interest to you:
 
-Hi, I’m Lorenz. Here are some facts about me that might be of interest to you: Hi, I’m Lorenz. Here are some facts about me that might be of interest to you:
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+* **Profession:** Software Engineer
+* **Degrees:**
+  * MSc (Dipl.Ing. for my fellow Austrians)
+  * BSc  
+  In Software Engineering & Management and Software Development & Business Management, respectively, from Graz University of Technology
+* **Interests:** Python, C++, many other programming languages, Linux, vim, free and open-source software
+* **Portfolio:** [lolei.github.io/portfolio](https://lolei.github.io/portfolio)
+* **How to reach me:** See the [front page](https://lolei.github.io) for either my email address or my socials
+
 `;
 
   return (
