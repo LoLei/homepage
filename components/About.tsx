4f43b78089adbbb6e066b0c08a0f04d7f52bfdc6
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-07 20:15:30 +0200

    Add About styles

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import styles from '../styles/About.module.scss';
 
 const About = (): JSX.Element => {
-  return <div>About</div>;
+  const markdown = `
+# About Me  
+
+Hi, I’m Lorenz. Here are some facts about me that might be of interest to you:
+
+Hi, I’m Lorenz. Here are some facts about me that might be of interest to you: Hi, I’m Lorenz. Here are some facts about me that might be of interest to you:
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+`;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+    </div>
+  );
 };
 
 export default About;
