53f5124ac66d7e351d20d598424211604f720619
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-07 17:52:48 +0200

    Add favicon, add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import '../styles/all.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <div className="container">
-      <div className="header">
-        <Header />
+    <>
+      <Head>
+        <title>Lorenz Leitner</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="main">
+          <Component {...pageProps} />
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
       </div>
-      <div className="main">
-        <Component {...pageProps} />
-      </div>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
+    </>
   );
 };
 
