c0c7d97f9be6d8e64d5cd6c1727e456390c57c49
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-07 19:15:50 +0200

    Add source link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import styles from '../styles/Footer.module.scss';
 
 const Footer = (): JSX.Element => {
-  return <footer className={styles.text}>Made with ❤ by Lorenz Leitner</footer>;
+  return (
+    <footer className={styles.style}>
+      <a href="https://github.com/LoLei/homepage" title="Source">
+        Made with ❤ by me
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
