852af6517c0289289f7c59f78691a1e5913a8651
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-10 11:05:08 +0200

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from '../styles/NotFound.module.scss';
+
+const NotFound = (): JSX.Element => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <h1>404 - Page Not Found</h1>
+      <div>
+        <div>
+          <span onClick={() => router.back()}><a>Back</a></span>
+        </div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
