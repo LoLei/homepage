2ef6752c8927ae8606db9caeaf89c2f668b2d9a3
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-12 15:55:30 +0200

    Make code syntax highlighting work

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,12 +2,42 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import styles from '../styles/Post.module.scss';
 import { IPost } from './Posts';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { funky } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import gfm from 'remark-gfm';
+
+/*eslint-disable */
+const components = {
+  // @ts-ignore
+  code({ node, inline, className, children, ...props }): JSX.Element {
+    const match = /language-(\w+)/.exec(className || '');
+    return !inline && match ? (
+      <SyntaxHighlighter
+        style={funky}
+        language={match[1]}
+        PreTag="div"
+        children={String(children).replace(/\n$/, '')}
+        {...props}
+      />
+    ) : (
+      <code className={className} {...props} />
+    );
+  },
+};
+/*eslint-enable */
 
 const Posts = (props: IProps): JSX.Element => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <ReactMarkdown>{props.postData.content}</ReactMarkdown>
+        <ReactMarkdown
+          plugins={[gfm]}
+          // @ts-ignore
+          // eslint-disable-next-line react/display-name
+          components={components}
+        >
+          {props.postData.content}
+        </ReactMarkdown>
       </div>
     </div>
   );
