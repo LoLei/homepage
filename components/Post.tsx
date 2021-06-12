d6f7e4fec57e0522cfe64aa6285ea00cf5d1f7f4
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-12 15:15:36 +0200

    Copy styling from about page

--- components/Post.tsx
+++ components/Post.tsx
@@ -6,10 +6,6 @@ import { IPost } from './Posts';
 const Posts = (props: IProps): JSX.Element => {
   return (
     <div className={styles.container}>
-      <div className={styles.heading}>
-        <h1>{props.postData.fileName}</h1>
-      </div>
-
       <div className={styles.content}>
         <ReactMarkdown>{props.postData.content}</ReactMarkdown>
       </div>
