e5adad36bf7394072720953f9b362af6946528e0
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-18 20:45:37 +0200

    Add db for post list first
    This caches the git API results

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,8 +1,8 @@
 import { GetServerSideProps } from 'next';
 import React from 'react';
 import Posts from '../../components/Posts';
+import Database from '../../util/db/Database';
 import { IRepositoryContentEntryMetadata } from '../../util/git/AbstractGitService';
-import GitDelegator from '../../util/git/GitDelegator';
 
 const PostsPage = (props: IProps): JSX.Element => {
   return <Posts postListings={props.postListings} />;
@@ -15,8 +15,13 @@ interface IProps {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const gitService = GitDelegator.Instance;
-  const posts = await gitService.getRepositoryContentList('https://github.com/LoLei/posts');
+  const database = Database.Instance;
+
+  if (await database.datastorePostListNeedsRepopulate()) {
+    await database.populatePostList();
+  }
+
+  const posts = await database.getPostList();
 
   if (posts == null || posts.length === 0) {
     return {
---
+++ util/db/Database.ts
@@ -0,0 +1,66 @@
+import AsyncNedb from 'nedb-async';
+import TimeDelta from '../date/delta';
+import { IRepositoryContentEntryMetadata } from '../git/AbstractGitService';
+import GitDelegator from '../git/GitDelegator';
+
+class Database {
+  private static _instance: Database;
+  private readonly datastorePostList: IInMemoryDataStore<IRepositoryContentEntryMetadata>;
+
+  private constructor() {
+    this.datastorePostList = {
+      creationDate: new Date(),
+      lastUpdatedDate: new Date(),
+      datastore: new AsyncNedb<IRepositoryContentEntryMetadata>(),
+    };
+  }
+
+  public static get Instance(): Database {
+    return this._instance || (this._instance = new this());
+  }
+
+  public async datastorePostListNeedsRepopulate(): Promise<boolean> {
+    // TODO: Move datastorePostList to separate class so this method can be reused
+    const age = TimeDelta.absoluteTimeDifferenceInHours(
+      this.datastorePostList.lastUpdatedDate,
+      new Date()
+    );
+    console.log(`Posts list needs repopulate in ${age}h`);
+    if ((await this.getPostList()).length === 0 || age >= 1) {
+      return true;
+    }
+    return false;
+  }
+
+  public async populatePostList(): Promise<IRepositoryContentEntryMetadata[]> {
+    console.log('Populating post list…');
+    // Empty datastore before
+    await this.datastorePostList.datastore.asyncRemove({}, { multi: true });
+
+    // Populate from Github
+    const gitService = GitDelegator.Instance;
+    const posts = await gitService.getRepositoryContentList('https://github.com/LoLei/posts');
+
+    // If the API call fails, do not repopulate the database and return the old entries
+    if (posts == null || posts.length === 0) {
+      console.warn('Git API call failed, skipping db repopulation, returning old results');
+      return this.getPostList();
+    }
+
+    const insertPromises = posts.map((p) => this.datastorePostList.datastore.asyncInsert(p));
+    this.datastorePostList.lastUpdatedDate = new Date();
+    return Promise.all(insertPromises);
+  }
+
+  public getPostList(): Promise<IRepositoryContentEntryMetadata[]> {
+    return this.datastorePostList.datastore.asyncFind({});
+  }
+}
+
+export default Database;
+
+interface IInMemoryDataStore<T> {
+  creationDate: Date;
+  lastUpdatedDate: Date;
+  datastore: AsyncNedb<T>;
+}
