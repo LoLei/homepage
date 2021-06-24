5de86eeca6800862f376428e87c4b168728105c8
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-24 15:56:54 +0200

    Update about page

--- components/About.tsx
+++ components/About.tsx
@@ -10,13 +10,13 @@ Hi, I’m Lorenz. Here are some facts about me that might be of interest to you:
 
 * **Profession:** Software Engineer
 * **Degrees:**
-  * MSc (Dipl.Ing. for my fellow Austrians)
-  * BSc  
-  In Software Engineering & Management and Software Development & Business Management, respectively, from Graz University of Technology
-* **Interests:** Python, C++, many other programming languages, Linux, vim, free and open-source software
-* **Portfolio:** [lolei.github.io/portfolio](https://lolei.github.io/portfolio)
-* **How to reach me:** See the [front page](https://lolei.github.io) for either my email address or my socials
-
+  * MSc (Dipl.Ing. in Austria) in [Software Engineering & Management](https://www.tugraz.at/en/studying-and-teaching/degree-and-certificate-programmes/masters-degree-programmes/software-engineering-and-management/)
+  * BSc in [Software Development & Business Management](https://www.tugraz.at/en/studying-and-teaching/degree-and-certificate-programmes/bachelors-degree-programmes/software-engineering-and-management/)  
+    From [Graz University of Technology](https://www.tugraz.at/en/home/)
+* **Interests:** 
+    * Python, C++, many other programming languages
+    * Linux, vim, ricing, ...
+    * Free and open-source software
 `;
 
   return (
